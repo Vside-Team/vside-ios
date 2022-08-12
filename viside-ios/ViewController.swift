fe1febb75e1b0f93448f35dd9d29015526be451d
jeuniii <jeuniis2@gmail.com>
2022-08-12 19:35:05 +0900

    Initial Commit

---
+++ viside-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  viside-ios
+//
+//  Created by 김정은 on 2022/08/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
